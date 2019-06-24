e68be4ca937cd231214f868ee1f0849763ceaf41
hongphuc2197 <hongphuc2197@gmail.com>
2019-06-24 17:33:06 +0700

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
+export default () => <div
+style={{ color: `purple` }}
+>
+<Link to="/contact/">Contact</Link>
+  <p>What a world.</p>
 
-export default () => <div>Hello world!</div>
+  <button class="primary-button">Click me</button>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
